1d381e658634ae72d68e8f2b012291bfe5a94c23
Alexandr Mykovnya <freonm@yandex.ru>
2021-08-03 19:26:33 +0300

    hotfix

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,8 +3,8 @@ const { validateUrl } = require('../errors/CelebrateUrlError');
 
 const validatePatchUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 });
 
